98761431d9f9f590bdfbcfc77dd074374bfb2822
Carmelo0603 <carmeloprovenzani1@gmail.com>
2026-04-17 09:39:06 +0200

    inizializzazione

---
+++ src/main/java/it/epicode/gestioneviaggi_s6_l5/GestioneViaggiS6L5Application.java
@@ -0,0 +1,13 @@
+package it.epicode.gestioneviaggi_s6_l5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneViaggiS6L5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioneViaggiS6L5Application.class, args);
+    }
+
+}
---
+++ src/test/java/it/epicode/gestioneviaggi_s6_l5/GestioneViaggiS6L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.gestioneviaggi_s6_l5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneViaggiS6L5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
